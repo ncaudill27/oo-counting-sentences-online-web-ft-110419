fbdf22a782a419d990ddd7ac0f70bd8e2313b10c
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-12 20:33:17 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    new = self.match(/[.!?]+/).split.count
+    new = self.split.match(/[.!?]+/).count
     binding.pry
     #self.split().count
     #"."||"!"||"?"
